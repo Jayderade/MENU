399f608ca456fbd61cebe702e26ffc055690cb3f
Jayderade <jcastleden@gmail.com>
2017-10-13 14:38:52 +1100

    IT WORKED

---
+++ Assets/Scripts/MusicScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicScript : MonoBehaviour {
+
+    public bool musicStart = false;
+    public GameObject musicPlayer;
+    public AudioSource music;
+
+    public void Awake()
+    {
+        if (!musicStart)
+        {
+            music.Play();
+            DontDestroyOnLoad(musicPlayer);
+            musicStart = true;
+        }
+        else
+        {
+            Destroy(musicPlayer);
+        }
+    }
+
+    
+}
